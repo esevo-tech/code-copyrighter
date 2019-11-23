d87da13b6039d661da0b939dc7fb74f67f7ec156
Elian Doran <elinnum@gmail.com>
2019-11-24 00:35:16 +0200

    Update directory structure

--- src/copyrighter.js
+++ src/copyrighter.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const input = fs.readFileSync("input.java").toString("utf8");
+const input = fs.readFileSync("../input.java").toString("utf8");
 const getFileHeader = require("./reader");
 const format = require("./formats/c-like.js");
 
