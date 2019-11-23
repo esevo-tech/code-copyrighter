b4ecf829618ed8439e5c5590d7f9787fb144404a
Elian Doran <elinnum@gmail.com>
2019-11-24 01:31:21 +0200

    Store scanned headers in an array

--- src/scanner.js
+++ src/scanner.js
@@ -4,18 +4,19 @@ const fs = require("fs");
 const getFileHeader = require("./reader");
 const format = require("./formats/c-like.js");
 
-function processSingleFile(path) {
-  const fileContent = fs.readFileSync(path).toString("utf8");
-  console.log(path);
-  const header = getFileHeader(fileContent, format);
-  console.log(header);
-}
-
 function scan(path) {
-  console.log("Scan: " + path);
+  headers = [];
+
+  function processSingleFile(path) {
+    const fileContent = fs.readFileSync(path).toString("utf8");
+    const header = getFileHeader(fileContent, format);
+    headers.push(header);
+  }
+
   glob(path, (err, files) => {
     for (file of files) {
       processSingleFile(file);
+      console.log(headers);
     }
   });
 }
