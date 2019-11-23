6d87779491761a255c334f581d2a60c9c2fa5459
Elian Doran <elinnum@gmail.com>
2019-11-24 00:02:00 +0200

    Match content of the header

--- copyrighter.js
+++ copyrighter.js
@@ -1,6 +1,12 @@
 const fs = require("fs");
-
-const regex = /^\/\*(.*?)\*\//s;
 const input = fs.readFileSync("input.java").toString("utf8");
 
-console.log(regex.exec(input)[1]);
+const headerRegex = /^\/\*(.*?)\*\//s;
+const headerContentRegex = /^\s*\*{0,1}[ \t]*(.*)$/gm;
+
+const headerContent = headerRegex.exec(input)[1];
+let headerContentMatch;
+
+while ((headerContentMatch = headerContentRegex.exec(headerContent)) != null) {
+  console.log(headerContentMatch[1]);
+}
