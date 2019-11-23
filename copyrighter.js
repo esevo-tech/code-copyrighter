a9b01a04aca0916cb2929bd2ea883cdc27097311
Elian Doran <elinnum@gmail.com>
2019-11-24 00:25:10 +0200

    Fix blank lines not being read

--- copyrighter.js
+++ copyrighter.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const input = fs.readFileSync("input.java").toString("utf8");
 
-const headerRegex = /^\/\*(.*?)\*\//s;
-const headerContentRegex = /^\s*\*{0,1}[ \t]*(.*)$/gm;
+const headerRegex = /^\/\*\s*(.*?)\s*\*\//sg;
+const headerContentRegex = /^[ \t]*\*{0,1}[ \t]*(.*)$/gm;
 
 const headerContent = headerRegex.exec(input)[1];
 let headerContentMatch;
