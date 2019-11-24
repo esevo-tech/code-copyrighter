13106cd49f07df1c24db38a2f074aa66837ade8d
Elian Doran <elinnum@gmail.com>
2019-11-24 11:52:34 +0200

    Remove old format

--- src/formats/c-like.js
+++
@@ -1,9 +0,0 @@
-const headerMatchAllComments = /^\/\*\s*(.*?)\s*\*\//sg;
-const headerMatchIgnoreJavadoc = /^\/\*{1}\s+(.*?)\s*\*\//sg;
-
-module.exports = {
-  reader: {
-    headerRegex: headerMatchIgnoreJavadoc,
-    headerContentRegex: /^[ \t]*\*{0,1}[ \t]*(.*)$/gm
-  }
-};
