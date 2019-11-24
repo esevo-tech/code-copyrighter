168731c3c24fc1c6128586bd07b04a8988c81b51
Elian Doran <elinnum@gmail.com>
2019-11-24 14:06:49 +0200

    Modularize year interpreter & fix single year match

--- src/interpreter/copyright-year-interpreter.js
+++ src/interpreter/copyright-year-interpreter.js
@@ -19,7 +19,7 @@ function parse(content) {
   const singleYearMatch = singleYearRegex.exec(content);
   if (singleYearMatch !== null) {
     content = content.replace(singleYearRegex, singleYearReplacement);
-    variables.year = rangeMatch[0];
+    variables.year = singleYearMatch[0];
   }
 
   return {
@@ -28,9 +28,4 @@ function parse(content) {
   };
 }
 
-const text = `
-Copyright 2018-2019 XYZ. All rights reserved.
-Copyright 2009 AFJA
-This Software has been designed by YYZ.
-`;
-console.log(parse(text));
+module.exports = parse;
