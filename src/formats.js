726ed51ea186eaba0e98e67c767ba46bfa32fc33
Elian Doran <elinnum@gmail.com>
2019-11-24 03:06:11 +0200

    Determine name of format based on file name

--- src/formats.js
+++ src/formats.js
@@ -2,16 +2,18 @@ const formatsGlob = "./config/formats/**.json";
 
 const fs = require("fs");
 const glob = require("glob");
+const path = require("path");
 
 function buildFormats() {
   const files = glob.sync(formatsGlob);
-  const formats = [];
+  const formats = {};
   for (const formatFile of files) {
     const format = JSON.parse(fs.readFileSync(formatFile).toString("utf8"));
+    const formatName = path.parse(formatFile).name;
     format.headerRegex = new RegExp(format.headerRegex).compile();
     format.headerContentRegex = new RegExp(format.headerContentRegex).compile();
 
-    formats.push(format);
+    formats[formatName] = format;
   }
 
   return formats;
