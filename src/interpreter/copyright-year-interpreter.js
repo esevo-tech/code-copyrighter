9c06b726fedad73c89a575c10a6d690e15f03e03
Elian Doran <elinnum@gmail.com>
2019-11-24 13:58:52 +0200

    Replace actual year values with placeholders

--- src/interpreter/copyright-year-interpreter.js
+++ src/interpreter/copyright-year-interpreter.js
@@ -1,14 +1,23 @@
-const dateRangeRegex = /(\d{4})[ *-–—]*(\d{4})/;
+const dateRangeRegex = /(\d{4})([ *-–—]*)(\d{4})/;
+const dateRangeReplacement = "{yearStart}$2{yearEnd}";
 
 function parse(content) {
+  let variables = {};
+
   // Date ranges (e.g. 2012-2018)
   const rangeMatch = dateRangeRegex.exec(content);
   if (rangeMatch !== null) {
-    return {
+    content = content.replace(dateRangeRegex, dateRangeReplacement);
+    variables = {
       yearStart: rangeMatch[1],
-      yearEnd: rangeMatch[2]
+      yearEnd: rangeMatch[3]
     }
   }
+
+  return {
+    content,
+    variables
+  };
 }
 
 const text = `
