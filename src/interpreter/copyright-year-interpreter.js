8c950feaff72366fed6190ab6f329c6bfa5981ff
Elian Doran <elinnum@gmail.com>
2019-11-30 17:01:08 +0200

    Parse single year and ranges into single variable

--- src/interpreter/copyright-year-interpreter.js
+++ src/interpreter/copyright-year-interpreter.js
@@ -1,25 +1,13 @@
-const dateRangeRegex = /(\d{4})([ *-–—]*)(\d{4})/;
-const dateRangeReplacement = "{yearStart}$2{yearEnd}";
-
-const singleYearRegex = /(\d{4})/;
-const singleYearReplacement = "{year}";
+const yearRegex = /[\d]+[\d ,\-–—]*[\d]+/gm;
+const yearReplacement = "{year}";
 
 function parse(content) {
   let variables = {};
-
-  // Date ranges (e.g. 2012-2018)
-  const rangeMatch = dateRangeRegex.exec(content);
-  if (rangeMatch !== null) {
-    content = content.replace(dateRangeRegex, dateRangeReplacement);
-    variables.yearStart = rangeMatch[1];
-    variables.yearEnd = rangeMatch[3];
-  }
   
-  // Single year
-  const singleYearMatch = singleYearRegex.exec(content);
-  if (singleYearMatch !== null) {
-    content = content.replace(singleYearRegex, singleYearReplacement);
-    variables.year = singleYearMatch[0];
+  const yearMatch = yearRegex.exec(content);
+  if (yearMatch !== null) {
+    content = content.replace(yearRegex, yearReplacement);
+    variables.year = yearMatch[0];
   }
 
   return {
